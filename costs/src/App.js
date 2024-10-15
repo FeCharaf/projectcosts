043ae7dc1812749ee2014e8e105de6e4ffb64136
FeCharaf <felipecharaf@gmail.com>
2024-10-15 09:59:43 -0300

    Add Navbar and Footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,30 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
+import Projects from './components/pages/Projects'
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/newproject">NewProject</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/projects" element={<Projects />}></Route>
           <Route exact path="/company" element={<Company />}></Route>
           <Route exact path="/contact" element={<Contact />}></Route>
           <Route exact path="/newproject" element={<NewProject />}></Route>
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
