81381e8e152548b19f3e64ea88112ea2da9ef845
FeCharaf <felipecharaf@gmail.com>
2024-11-17 18:14:08 -0300

    Adicionar novos serviços

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from "uuid";
+
 import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -67,8 +69,44 @@ function Project() {
       .catch((err) => console.log(err));
   }
 
-  function createService() {
-    
+  function createService(project) {
+    // last service
+    const lastService = project.services[project.services.length - 1];
+
+    lastService.id = uuidv4();
+
+    const lastServiceCost = lastService.cost;
+
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+    // maximum value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage("Orçamento ultrapassado, verifique o valor do serviço");
+      setType("error");
+      project.services.pop();
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
+      return false;
+    }
+
+    // Add service cost to total cost
+    project.cost = newCost;
+
+    // Update project
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        // exibir serviço
+        console.log(data)
+      })
+      .catch((err) => console.log(err));
   }
 
   function toggleProjectForm() {
@@ -99,7 +137,7 @@ function Project() {
                     <span>Categoria:</span> {project.category.name}
                   </p>
                   <p>
-                    <span>Total do Orçamento:</span> {project.budget}
+                    <span>Total do Orçamento:</span> R${project.budget}
                   </p>
                   <p>
                     <span>Total Utilizado:</span> R${project.cost}
@@ -116,7 +154,7 @@ function Project() {
                 </div>
               )}
             </div>
-            
+
             <div className={styles.service_form_container}>
               <h2>Adicione um serviço:</h2>
               <button onClick={toggleServiceForm} className={styles.btn}>
@@ -125,14 +163,15 @@ function Project() {
               <div className={styles.project_info}>
                 {/* FORMULARIO DE SERVIÇOS */}
                 {showServiceForm && (
-                  <ServiceForm handleSubmit={createService} 
-                  btnText="Adicionar Serviço"
-                  projectData={project}
+                  <ServiceForm
+                    handleSubmit={createService}
+                    btnText="Adicionar Serviço"
+                    projectData={project}
                   />
-                ) }
+                )}
               </div>
             </div>
-              {/* Area de Serviços */}
+            {/* Area de Serviços */}
             <h2>Serviços</h2>
             <Container customClass="start">
               <p>Serviços</p>
